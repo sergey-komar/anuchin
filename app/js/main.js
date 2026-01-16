14792f31b40b5a56f87e9021faa12b8decff3e99
sergey-komar <kursev81@yandex.ru>
2026-01-16 11:03:31 +0300

    start mobile

--- app/js/main.js
+++ app/js/main.js
@@ -29,6 +29,15 @@ $(function () {
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    const menu = document.querySelector(".menu");
+    const mobile = document.querySelector(".nav-icon");
+
+    mobile.addEventListener("click", function () {
+      this.classList.toggle("nav-icon--active");
+      menu.classList.toggle("nav--active");
+      document.body.classList.toggle('overflow')
+    });
+
 
      const headerShadow = document.querySelector('.header-top');
      window.addEventListener('scroll', () => {
